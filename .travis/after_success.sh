73a078817b7273ed772e7b3e676a8de5fdbb5d52
shiraji <isogai.shiraji@gmail.com>
2016-04-24 00:36:06 +0900

    Initial commit

---
+++ .travis/after_success.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Travis ci script for releasing gradle project
+# Adapted from http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+# Change this
+REPO="find-pull-request"
+
+# Change if necessary
+USER="shiraji"
+JDK="oraclejdk7"
+BRANCH="master"
+
+# Not handling errors
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$USER/$REPO" ]; then
+  # Check repo
+  echo "TRAVIS_REPO_SLUG: '$TRAVIS_REPO_SLUG' USER/REPO: '$USER/$REPO'"
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  # Check JDK
+  echo "TRAVIS_JDK_VERSION: '$TRAVIS_JDK_VERSION' JDK: '$JDK"
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  # Check how to run this script
+  echo "It's pull request!"
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  # Check branch
+  echo "TRAVIS_BRANCH: '$TRAVIS_BRANCH' BRANCH: '$BRANCH'"
+elif [ "$IDEA_VERSION" != "LATEST-EAP-SNAPSHOT" ]; then
+  # Check IDEA version
+  echo "IDEA_VERSION: '$IDEA_VERSION'"
+else
+  # Without snapshot
+  if [ -f .travis/release ]; then
+    echo "Start releasing..."
+    ./gradlew publishPlugin
+  else
+    echo "No .travis/release file"
+  fi
+fi
---
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/GeneratePMCodeAction.kt
@@ -0,0 +1,13 @@
+package com.github.shiraji.permissionsdispatcherplugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GeneratePMCodeAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent?) {
+    }
+
+    override fun update(e: AnActionEvent?) {
+        super.update(e)
+    }
+}
