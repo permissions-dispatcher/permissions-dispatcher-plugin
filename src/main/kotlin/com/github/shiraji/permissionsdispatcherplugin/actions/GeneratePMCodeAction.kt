e83322c5a16db34f1d7bcb8eb6fd1a69f39ccede
shiraji <isogai.shiraji@gmail.com>
2016-04-26 01:07:26 +0900

    Add update method that enable only Activity or Fragment

--- src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/actions/GeneratePMCodeAction.kt
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/actions/GeneratePMCodeAction.kt
@@ -3,6 +3,8 @@ package com.github.shiraji.permissionsdispatcherplugin.actions
 import com.github.shiraji.permissionsdispatcherplugin.models.GeneratePMCodeModel
 import com.intellij.codeInsight.CodeInsightActionHandler
 import com.intellij.codeInsight.actions.CodeInsightAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
@@ -11,6 +13,21 @@ import com.intellij.psi.PsiJavaFile
 class GeneratePMCodeAction : CodeInsightAction() {
     override fun getHandler(): CodeInsightActionHandler {
         return Handler()
+    }
+
+    override fun update(e: AnActionEvent?) {
+        e ?: return
+        super.update(e)
+
+        val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
+        if (file !is PsiJavaFile) return
+
+        val project = e.getData(CommonDataKeys.PROJECT) ?: return
+        val model = GeneratePMCodeModel(project)
+
+        e.presentation.isEnabledAndVisible = model.isActivity(file.classes[0])
+                || model.isSupportFragment(file.classes[0])
+                || model.isFragment(file.classes[0])
     }
 }
 
