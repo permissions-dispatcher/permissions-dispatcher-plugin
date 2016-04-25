2c27b19cda505e2b8f0f07d5a6456af9a8a03356
shiraji <isogai.shiraji@gmail.com>
2016-04-26 00:13:59 +0900

    Add features that find if the class is Activity or Fragment

--- src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/models/GeneratePMCodeModel.kt
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/models/GeneratePMCodeModel.kt
@@ -6,6 +6,22 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.search.GlobalSearchScope
 
 class GeneratePMCodeModel {
+
+    fun isActivity(aClass: PsiClass, project: Project): Boolean {
+        val activity = createPsiClass("android.app.Activity", project) ?: return false
+        return aClass.isInheritor(activity, true)
+    }
+
+    fun isFragment(aClass: PsiClass, project: Project): Boolean {
+        val fragment = createPsiClass("android.app.Fragment", project) ?: return false
+        return aClass.isInheritor(fragment, true)
+    }
+
+    fun isSupportFragment(aClass: PsiClass, project: Project): Boolean {
+        val fragment = createPsiClass("android.support.v4.app.Fragment", project) ?: return false
+        return aClass.isInheritor(fragment, true)
+    }
+
     fun createPsiClass(qualifiedName: String, project: Project): PsiClass? {
         val psiFacade = JavaPsiFacade.getInstance(project);
         val searchScope = GlobalSearchScope.allScope(project);
