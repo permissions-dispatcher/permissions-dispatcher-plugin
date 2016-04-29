aad25133cf63318154946b20d6439c1147c4bf54
shiraji <isogai.shiraji@gmail.com>
2016-04-29 10:42:23 +0900

    Make handler in separate class

--- src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/actions/GeneratePMCodeAction.kt
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/actions/GeneratePMCodeAction.kt
@@ -1,20 +1,17 @@
 package com.github.shiraji.permissionsdispatcherplugin.actions
 
+import com.github.shiraji.permissionsdispatcherplugin.handlers.GeneratePMCodeHandler
 import com.github.shiraji.permissionsdispatcherplugin.models.GeneratePMCodeModel
 import com.intellij.codeInsight.CodeInsightActionHandler
 import com.intellij.codeInsight.actions.CodeInsightAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiJavaFile
 import javax.swing.JOptionPane
 
 class GeneratePMCodeAction : CodeInsightAction() {
     override fun getHandler(): CodeInsightActionHandler {
-        return Handler()
+        return GeneratePMCodeHandler()
     }
 
     override fun update(e: AnActionEvent?) {
@@ -38,50 +35,4 @@ class GeneratePMCodeAction : CodeInsightAction() {
             super.actionPerformed(e)
         }
     }
-}
-
-class Handler : CodeInsightActionHandler {
-    override fun startInWriteAction() = true
-
-    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
-        if (file !is PsiJavaFile) return
-        val model = GeneratePMCodeModel(project)
-        addRuntimePermissionAnnotation(file, model, project)
-        addNeedsPermissionMethod(file, model, project)
-        addOnRequestPermissionsResult(file, model, project)
-    }
-
-    private fun addOnRequestPermissionsResult(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
-        val methods = file.classes[0].findMethodsByName("onRequestPermissionsResult", false)
-
-        if (methods.size == 0) {
-            val methodTemplate = """public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                ${file.classes[0].name}PermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
-            }""".trimMargin()
-
-            val method = JavaPsiFacade.getElementFactory(project).createMethodFromText(methodTemplate, file.classes[0])
-            method.modifierList.addAnnotation("Override")
-            file.classes[0].add(method)
-        } else {
-            // TODO check there is XxxPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults); and if not, add the line
-        }
-    }
-
-    private fun addNeedsPermissionMethod(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
-        val methodTemplate = """void ${model.methodName}() {
-        }""".trimMargin()
-
-        val method = JavaPsiFacade.getElementFactory(project).createMethodFromText(methodTemplate, file.classes[0])
-        method.modifierList.addAnnotation("NeedsPermission(${model.toPermissionParameter()})")
-        file.importClass(model.createPsiClass("permissions.dispatcher.NeedsPermission", project))
-        file.classes[0].add(method)
-    }
-
-    private fun addRuntimePermissionAnnotation(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
-        if (file.classes[0].modifierList?.findAnnotation("permissions.dispatcher.RuntimePermissions") != null) return
-        file.classes[0].modifierList?.addAnnotation("RuntimePermissions")
-        file.importClass(model.createPsiClass("permissions.dispatcher.RuntimePermissions", project))
-    }
-
 }
---
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/handlers/GeneratePMCodeHandler.kt
@@ -0,0 +1,55 @@
+package com.github.shiraji.permissionsdispatcherplugin.handlers
+
+import com.github.shiraji.permissionsdispatcherplugin.models.GeneratePMCodeModel
+import com.intellij.codeInsight.CodeInsightActionHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
+
+class GeneratePMCodeHandler : CodeInsightActionHandler {
+    override fun startInWriteAction() = true
+
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        if (file !is PsiJavaFile) return
+        val model = GeneratePMCodeModel(project)
+        addRuntimePermissionAnnotation(file, model, project)
+        addNeedsPermissionMethod(file, model, project)
+        addOnRequestPermissionsResult(file, model, project)
+    }
+
+    private fun addOnRequestPermissionsResult(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
+        val methods = file.classes[0].findMethodsByName("onRequestPermissionsResult", false)
+
+        if (methods.size == 0) {
+            val methodTemplate = """public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                ${file.classes[0].name}PermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+            }""".trimMargin()
+
+            val method = JavaPsiFacade.getElementFactory(project).createMethodFromText(methodTemplate, file.classes[0])
+            method.modifierList.addAnnotation("Override")
+            file.classes[0].add(method)
+        } else {
+            // TODO check there is XxxPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults); and if not, add the line
+        }
+    }
+
+    private fun addNeedsPermissionMethod(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
+        val methodTemplate = """void ${model.methodName}() {
+        }""".trimMargin()
+
+        val method = JavaPsiFacade.getElementFactory(project).createMethodFromText(methodTemplate, file.classes[0])
+        method.modifierList.addAnnotation("NeedsPermission(${model.toPermissionParameter()})")
+        file.importClass(model.createPsiClass("permissions.dispatcher.NeedsPermission", project))
+        file.classes[0].add(method)
+    }
+
+    private fun addRuntimePermissionAnnotation(file: PsiJavaFile, model: GeneratePMCodeModel, project: Project) {
+        if (file.classes[0].modifierList?.findAnnotation("permissions.dispatcher.RuntimePermissions") != null) return
+        file.classes[0].modifierList?.addAnnotation("RuntimePermissions")
+        file.importClass(model.createPsiClass("permissions.dispatcher.RuntimePermissions", project))
+    }
+
+}
