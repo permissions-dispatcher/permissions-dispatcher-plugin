d74e683ad93266132a3a8da9010eee1e7cd478ee
shiraji <isogai.shiraji@gmail.com>
2016-04-30 14:46:28 +0900

    Created delegate class for JDialog

---
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/views/GeneratePMCodeDialogDelegate.kt
@@ -0,0 +1,77 @@
+package com.github.shiraji.permissionsdispatcherplugin.views
+
+import java.awt.event.KeyEvent
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import javax.swing.JCheckBox
+import javax.swing.JComponent
+import javax.swing.KeyStroke
+import javax.swing.WindowConstants
+
+class GeneratePMCodeDialogDelegate(val dialog: GeneratePMCodeDialog) {
+
+    val dengarPermissionsCheckbox = listOf<JCheckBox>(
+            dialog.readCalendar, dialog.writeCalendar, dialog.camera, dialog.readContacts,
+            dialog.writeContacts, dialog.getAccounts, dialog.accessFineLocation,
+            dialog.accessCoarseLocation, dialog.recordAudio, dialog.readPhoneState,
+            dialog.callPhone, dialog.readCallLog, dialog.writeCallLog, dialog.addVoicemail,
+            dialog.useSip, dialog.processOutgoingCall, dialog.bodySensors, dialog.sendSms,
+            dialog.receiveSms, dialog.readSms, dialog.receiveWapPush, dialog.receiveMms,
+            dialog.readExternalStorage, dialog.writeExternalStorage)
+
+    val specialPermissionsCheckbox = listOf<JCheckBox>(dialog.systemAlertWindow, dialog.writeSettings)
+
+    fun initDialog() {
+        dialog.apply {
+            setContentPane(contentPane)
+            isModal = true
+            defaultCloseOperation = WindowConstants.DO_NOTHING_ON_CLOSE
+            getRootPane().defaultButton = buttonOK
+            buttonOK.addActionListener({
+                isOk = true
+                isVisible = false
+            })
+            buttonCancel.addActionListener({ onCancel() })
+            addWindowListener(object : WindowAdapter() {
+                override fun windowClosing(e: WindowEvent?) {
+                    onCancel()
+                }
+            })
+
+            // call onCancel() on ESCAPE
+            contentPane.registerKeyboardAction({ onCancel() }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        }
+
+        dengarPermissionsCheckbox.forEach {
+            it.addChangeListener {
+                val checkBox: JCheckBox = it.source as JCheckBox
+                if (checkBox.isSelected) {
+                    specialPermissionsCheckbox.forEach {
+                        it.isSelected = false
+                    }
+                }
+            }
+        }
+
+        specialPermissionsCheckbox.forEach {
+            it.addChangeListener {
+                val checkBox: JCheckBox = it.source as JCheckBox
+                if (checkBox.isSelected) {
+                    dengarPermissionsCheckbox.forEach {
+                        it.isSelected = false
+                    }
+
+                    specialPermissionsCheckbox.filterNot {
+                        it.equals(checkBox)
+                    }.forEach {
+                        it.isSelected = false
+                    }
+                }
+            }
+        }
+    }
+
+    private fun onCancel() {
+        dialog.isVisible = false
+    }
+}
