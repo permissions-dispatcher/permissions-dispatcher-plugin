078af6e6151614ea0f41d8c3dc5454be35382e18
shiraji <isogai.shiraji@gmail.com>
2017-01-11 00:31:20 +0900

    Remove unused version data

--- src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/data/PdVersion.kt
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/data/PdVersion.kt
@@ -2,14 +2,12 @@ package com.github.shiraji.permissionsdispatcherplugin.data
 
 enum class PdVersion() {
     VERSION_2_1_3,
-    VERSION_2_2_0,
     UNKNOWN;
 
     companion object {
         fun fromText(versionText: String): PdVersion {
             return when (versionText) {
                 "2.1.3" -> VERSION_2_1_3
-                "2.2.0" -> VERSION_2_2_0
                 else -> UNKNOWN
             }
         }
