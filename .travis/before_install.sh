31639fc9258012a15dad9dccbe16e176a567189c
shiraji <isogai.shiraji@gmail.com>
2017-01-13 15:21:58 +0900

    Fix travis script for PR

---
+++ .travis/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  # Check how to run this script
+  echo "It's pull request!"
+else
+  # Without snapshot
+  openssl aes-256-cbc -K $encrypted_62bba9a47a08_key -iv $encrypted_62bba9a47a08_iv -in travis.enc -out .travis.ssh -d
+  chmod 600 .travis.ssh
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile .travis.ssh\n" >> ~/.ssh/config
+fi
