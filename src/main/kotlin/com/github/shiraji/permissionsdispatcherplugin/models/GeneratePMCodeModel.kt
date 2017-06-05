1bf5931546558584cf6fdddc3ab7187d1616c21e
shiraji <isogai.shiraji@gmail.com>
2017-06-05 11:48:21 -0400

    Remove duplicate methods

--- src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/models/GeneratePMCodeModel.kt
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/models/GeneratePMCodeModel.kt
@@ -1,15 +1,14 @@
 package com.github.shiraji.permissionsdispatcherplugin.models
 
+import com.github.shiraji.permissionsdispatcherplugin.extentions.createPsiClass
 import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
-import com.intellij.psi.search.GlobalSearchScope
 
 class GeneratePMCodeModel(val project: Project) {
 
-    private val activityPsiClass: PsiClass? = createPsiClass("android.app.Activity")
-    private val fragmentPsiClass: PsiClass? = createPsiClass("android.app.Fragment")
-    private val supportFragmentPsiClass: PsiClass? = createPsiClass("android.support.v4.app.Fragment")
+    private val activityPsiClass: PsiClass? = project.createPsiClass("android.app.Activity")
+    private val fragmentPsiClass: PsiClass? = project.createPsiClass("android.app.Fragment")
+    private val supportFragmentPsiClass: PsiClass? = project.createPsiClass("android.support.v4.app.Fragment")
 
     private val specialPermissionNames = listOf("SYSTEM_ALERT_WINDOW", "WRITE_SETTINGS")
 
@@ -38,12 +37,6 @@ class GeneratePMCodeModel(val project: Project) {
     }
 
     fun isActivityOrFragment(aClass: PsiClass) = isActivity(aClass) || isFragment(aClass) || isSupportFragment(aClass)
-
-    fun createPsiClass(qualifiedName: String): PsiClass? {
-        val psiFacade = JavaPsiFacade.getInstance(project)
-        val searchScope = GlobalSearchScope.allScope(project)
-        return psiFacade.findClass(qualifiedName, searchScope)
-    }
 
     fun toPermissionParameter(): String {
         return when (permissions.size) {
