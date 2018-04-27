842b9f7e7441b45bcb49533ea2ef97bf02090aab
shiraji <isogai.shiraji@gmail.com>
2018-04-27 17:32:31 +0900

    foo

---
+++ src/main/kotlin/com/github/shiraji/permissionsdispatcherplugin/inspections/AddMissingOnRequestPermissionsResultInspection.kt
@@ -0,0 +1,10 @@
+package com.github.shiraji.permissionsdispatcherplugin.inspections
+
+import com.intellij.codeInspection.BaseJavaLocalInspectionTool
+
+class AddMissingOnRequestPermissionsResultInspection : BaseJavaLocalInspectionTool() {
+
+
+
+
+}
